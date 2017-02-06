2dca1bed30be64bc1c3f238051f6fcc989d30122
jerguslejko <jergus.lejko@gmail.com>
2017-02-06 02:21:06 +0000

    Tweak git aliases

--- aliases/git.zsh
+++ aliases/git.zsh
@@ -5,19 +5,16 @@ alias gs="git status"
 alias gc="git commit -m"
 alias ga="git add"
 alias gaa="git add --all ."
-alias gwip="git add . && git commit -m 'wip'"
-alias glast="git reset HEAD~"
-alias gec="git commit --allow-empty -m 'Initial commit'"
+alias gwip="git add --all . && git commit -m 'wip'"
 
 alias gam="git commit --amend --no-edit"
 alias gamm="git commit --amend"
 
-alias gnah="git checkout -- . && git clean -fdq"
+alias gnah="git reset --quiet HEAD . && git checkout -- . && git clean -fdq"
 
 # Logs and diffs
 alias gl="git log --oneline"
-alias gd="git diff -- . ':!composer.lock' ':!yarn.lock'"
-alias gds="git diff --staged -- . ':!composer.lock' ':!yarn.lock'"
+alias gd="git diff HEAD -- . ':!composer.lock' ':!yarn.lock'"
 
 # Branches
 alias gb="git branch"
