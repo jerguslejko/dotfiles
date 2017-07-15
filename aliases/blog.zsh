93e0f071ed0f6d4206b96e1b686fb101c1aafcca
jerguslejko <jergus.lejko@gmail.com>
2017-07-16 00:29:47 +0200

    move blog aliases

--- aliases/blog.zsh
+++
@@ -1,3 +0,0 @@
-alias j="jekyll"
-
-alias blog="cd ~/Blog"
--- aliases/macos.zsh
+++ aliases/macos.zsh
@@ -10,3 +10,7 @@ alias rm="/usr/local/bin/trash"
 
 # Helpers
 alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Your public SSH key was successfully copied to the clipboard.'"
+
+# Blog
+alias j="jekyll"
+alias blog="cd ~/Blog"
