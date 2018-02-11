bbef32472283731b83245693543820e4eefb6c2e
Jerguš Lejko <jerguslejko@users.noreply.github.com>
2018-02-11 21:21:29 +0000

    Update general.zsh

--- aliases/general.zsh
+++ aliases/general.zsh
@@ -2,12 +2,11 @@
 alias c="clear"
 alias l="ls -lAh"
 alias -g @="| grep -i"
-alias sudo="sudo " # this enables running aliases with sudo
+alias sudo="sudo "
 alias _="sudo"
-alias __='sudo $(fc -ln -1)'
 
 # Other
 t() {
-    tree -I '.git|.svn|.DS_Store|node_modules|vendor' --dirsfirst -L "${2-3}" -aC "${1-.}"
+    tree -I '.git|.DS_Store|node_modules|vendor' --dirsfirst -L "${2-3}" -aC "${1-.}"
 }
 
