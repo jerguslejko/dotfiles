b721247815f941cd1ff94106e5339aa9bd83f48b
jerguslejko <jergus.lejko@gmail.com>
2018-04-13 22:55:20 +0100

    move lines

--- aliases/git.zsh
+++ aliases/git.zsh
@@ -5,20 +5,20 @@ alias gs="git status"
 alias gc="git commit -m"
 alias ga="git add"
 alias gaa="git add --all ."
-alias gwip="git add --all . && git commit -m 'WIP'"
 alias gac="git commit -am"
+alias gwip="git add --all . && git commit -m 'WIP'"
 
 alias gr="git rebase"
 alias grm="git rebase master"
 
-alias gam="git commit --amend --no-edit"
 alias gamm="git commit --amend"
+alias gam="git commit --amend --no-edit"
 
 alias gnah="git reset --quiet HEAD . && git checkout -- . && git clean -fdq"
 
 # Logs and diffs
-alias gl="git log --oneline"
 alias gd="git diff"
+alias gl="git log --oneline"
 
 # Branches
 alias gb="git branch"
