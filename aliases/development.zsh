e2267a3b63ab2151c77648c52f1ed973b9960b22
jerguslejko <jergus.lejko@gmail.com>
2018-04-13 23:00:52 +0100

    remove alias

--- aliases/development.zsh
+++ aliases/development.zsh
@@ -3,7 +3,6 @@ alias art="php artisan"
 alias fresh="php artisan migrate:fresh --seed"
 alias tink="[[ -f ./artisan ]] && php artisan tinker --verbose || psysh"
 
-alias dusk="php artisan dusk"
 alias phpunit="./vendor/bin/phpunit"
 alias coverage="php -dzend_extension='/usr/local/opt/php72-xdebug/xdebug.so' vendor/bin/phpunit --coverage-html"
 alias pf="./vendor/bin/phpunit --filter"
