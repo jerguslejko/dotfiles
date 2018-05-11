9de6b74f2f378206e7404d0b84de3e35acc91efd
jerguslejko <jergus.lejko@gmail.com>
2018-05-11 17:19:49 +0100

    change xdebug path

--- aliases/development.zsh
+++ aliases/development.zsh
@@ -4,7 +4,7 @@ alias fresh="php artisan migrate:fresh --seed"
 alias tink="[[ -f ./artisan ]] && php artisan tinker --verbose || psysh"
 
 alias phpunit="./vendor/bin/phpunit"
-alias coverage="php -dzend_extension='/usr/local/opt/php72-xdebug/xdebug.so' vendor/bin/phpunit --coverage-html"
+alias coverage="php -dzend_extension='/usr/local/Cellar/php/7.2.5/pecl/20170718/xdebug.so' vendor/bin/phpunit --coverage-html"
 alias pf="./vendor/bin/phpunit --filter"
 alias p="phpunit --stop-on-fail"
 
