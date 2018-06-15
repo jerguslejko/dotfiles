5b43d2b40ba133b4bc201a1674b4781281477598
jerguslejko <jergus.lejko@gmail.com>
2018-06-15 13:47:38 +0200

    KILL THE FUCKING TOUCHBAR

--- aliases/macos.zsh
+++ aliases/macos.zsh
@@ -12,3 +12,4 @@ alias rm="/usr/local/bin/trash" # brew install trash
 alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Your public SSH key was successfully copied to the clipboard.'"
 alias show_hidden="defaults write com.apple.finder AppleShowAllFiles -bool YES && killall Finder"
 alias hide_hidden="defaults write com.apple.finder AppleShowAllFiles -bool NO  && killall Finder"
+alias kill_touchbar="pkill 'Touch Bar agent'; killall 'ControlStrip'"
