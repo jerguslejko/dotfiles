00ce9d3aad41994654410120888ee83f20b7a494
jerguslejko <jergus.lejko@gmail.com>
2018-06-15 13:47:43 +0200

    add alias for composer

--- aliases/development.zsh
+++ aliases/development.zsh
@@ -7,6 +7,7 @@ alias phpunit="./vendor/bin/phpunit"
 alias coverage="php -dzend_extension='/usr/local/Cellar/php/7.2.5/pecl/20170718/xdebug.so' vendor/bin/phpunit --coverage-html"
 alias pf="./vendor/bin/phpunit --filter"
 alias p="phpunit --stop-on-fail"
+alias com="composer"
 
 # Others
 alias d=docker
