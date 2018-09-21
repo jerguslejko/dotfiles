6e5aaf1a9e353326cb2fdc10ddc819f0e4030bfe
jerguslejko <jergus.lejko@gmail.com>
2018-09-21 13:29:06 +0200

    export Code folder

--- aliases/macos.zsh
+++ aliases/macos.zsh
@@ -1,9 +1,8 @@
 # Movement
-alias code="cd ~/Code"
+alias dot="cd ~/.dotfiles"
 alias desk="cd ~/Desktop"
 alias docs="cd ~/Documents"
 alias down="cd ~/Downloads"
-alias dot="cd ~/.dotfiles"
 
 # Tools
 alias b="brew"
@@ -15,3 +14,6 @@ alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Your public SSH key was su
 alias show_hidden="defaults write com.apple.finder AppleShowAllFiles -bool YES && killall Finder"
 alias hide_hidden="defaults write com.apple.finder AppleShowAllFiles -bool NO  && killall Finder"
 alias kill_touchbar="pkill 'Touch Bar agent'; killall 'ControlStrip'"
+
+# Expose ~/Code folder
+cdpath=($cdpath ~/Code)
