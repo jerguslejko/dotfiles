447092968246453e6947efb96f4503e9a8c51b8a
jerguslejko <jergus.lejko@gmail.com>
2018-09-21 13:33:04 +0200

    use prefixes

--- aliases/macos.zsh
+++ aliases/macos.zsh
@@ -6,8 +6,8 @@ alias down="cd ~/Downloads"
 
 # Tools
 alias b="brew"
-alias rm="/usr/local/bin/trash" # brew install trash
-alias cat="bat --paging never --plain" # brew install bat
+alias rm="$(brew --prefix)/bin/trash" # brew install trash
+alias cat="$(brew --prefix)/bin/bat --paging never --plain" # brew install bat
 
 # Helpers
 alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Your public SSH key was successfully copied to the clipboard.'"
