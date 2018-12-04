68662a5550c17c3124a2f0c0951049281f298c55
jerguslejko <jergus.lejko@gmail.com>
2018-12-04 09:28:04 +0000

    remove alias

--- aliases/development.zsh
+++ aliases/development.zsh
@@ -6,7 +6,6 @@ alias tink="[[ -f ./artisan ]] && php artisan tinker --verbose || psysh"
 alias phpunit="./vendor/bin/phpunit"
 alias pf="phpunit --stop-on-fail --filter"
 alias p="phpunit --stop-on-fail"
-alias coverage="php -dzend_extension='/usr/local/opt/php/pecl/20170718/xdebug.so' vendor/bin/phpunit --coverage-html"
 alias com="composer"
 
 # Others
