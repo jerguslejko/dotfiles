e83c055c91d6800c4f5cf0756d469c7cc8fd52ff
jerguslejko <jergus.lejko@gmail.com>
2019-03-09 17:06:47 +0000

    move alias

--- aliases/macos.zsh
+++ aliases/macos.zsh
@@ -1,6 +1,7 @@
 # Tools
 alias rm="$(brew --prefix)/bin/trash" # brew install trash
 alias cat="$(brew --prefix)/bin/bat --paging never --plain" # brew install bat
+alias mysql="$(brew --prefix)/bin/mycli --host=localhost" # brew install mycli
 
 # Helpers
 alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Your public SSH key was successfully copied to the clipboard.'"
@@ -14,5 +15,3 @@ alias kill_touchbar="pkill 'Touch Bar agent'; killall 'ControlStrip'"
 
 alias dark="osascript -e 'tell application \"System Events\" to tell appearance preferences to set dark mode to not dark mode'"
 alias light="osascript -e 'tell application \"System Events\" to tell appearance preferences to set dark mode to not dark mode'"
-
-alias mysql="mycli --host=localhost"
