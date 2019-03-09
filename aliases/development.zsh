6fd4971840663bc2abf078e7c88b9a2f403ae066
jerguslejko <jergus.lejko@gmail.com>
2019-03-09 17:05:45 +0000

    lint

--- aliases/development.zsh
+++ aliases/development.zsh
@@ -7,9 +7,7 @@ alias phpunit="./vendor/bin/phpunit"
 alias pf="phpunit --stop-on-fail --filter"
 alias p="phpunit --stop-on-fail"
 alias com="composer"
-
-# Others
-alias y=yarn
+alias y="yarn"
 
 t() {
     tree -I '.git|.DS_Store|node_modules|vendor' --dirsfirst -L "${2-100}" -aC "${1-.}"
