43d15af8e5760b31140eaee3055cfdb6b0580aba
jerguslejko <jergus.lejko@gmail.com>
2019-03-18 17:34:44 +0000

    updates

--- aliases/development.zsh
+++ aliases/development.zsh
@@ -6,8 +6,6 @@ alias tink="[[ -f ./artisan ]] && php artisan tinker --verbose || psysh"
 alias phpunit="./vendor/bin/phpunit"
 alias pf="phpunit --stop-on-fail --filter"
 alias p="phpunit --stop-on-fail"
-alias com="composer"
-alias y="yarn"
 
 t() {
     tree -I '.git|.DS_Store|node_modules|vendor' --dirsfirst -L "${2-100}" -aC "${1-.}"
