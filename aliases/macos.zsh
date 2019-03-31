00d847a8c15a825dbbd8052bed39849a0f91564b
jerguslejko <jergus.lejko@gmail.com>
2019-03-31 11:42:08 +0100

    add "cat=bat" now supports dark mode

--- aliases/macos.zsh
+++ aliases/macos.zsh
@@ -1,6 +1,5 @@
 # Tools
 alias rm="$(brew --prefix)/bin/trash" # brew install trash
-alias cat="$(brew --prefix)/bin/bat --paging never --plain" # brew install bat
 alias mysql="$(brew --prefix)/bin/mycli --host=localhost" # brew install mycli
 
 # Hidden files
@@ -10,6 +9,12 @@ alias hide_hidden="defaults write com.apple.finder AppleShowAllFiles -bool NO  &
 # macOS is great!
 alias kill_touchbar="pkill 'Touch Bar agent'; killall 'ControlStrip'"
 
-# light/dark mode
-alias dark="osascript -e 'tell application \"System Events\" to tell appearance preferences to set dark mode to not dark mode'"
-alias light="osascript -e 'tell application \"System Events\" to tell appearance preferences to set dark mode to not dark mode'"
+# brew install bat
+bat() {
+    $(brew --prefix)/bin/bat \
+        --paging never \
+        --plain \
+        --theme=$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub) \
+        $1
+}
+alias cat="bat"
