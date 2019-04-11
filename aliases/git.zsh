a348daa9ab54c99c731b49f60c1744ac8bf96f1c
jerguslejko <jergus.lejko@gmail.com>
2019-04-11 18:46:07 +0100

    move alias

--- aliases/git.zsh
+++ aliases/git.zsh
@@ -9,8 +9,6 @@ alias gwip="git add --all . && git commit -m 'WIP'"
 alias gamm="git commit --amend"
 alias gam="git commit --amend --no-edit"
 
-alias gnah="git reset --quiet HEAD . && git checkout -- . && git clean -fdq"
-
 # Logs and diffs
 alias gd="git diff"
 alias gl="git log --oneline"
