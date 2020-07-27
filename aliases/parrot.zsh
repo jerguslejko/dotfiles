4ab903d14b204cb626482000d24eba301224e158
jerguslejko <jergus.lejko@gmail.com>
2020-07-27 16:08:38 +0200

    parrot stuff

---
+++ aliases/parrot.zsh
@@ -0,0 +1,6 @@
+alias py="python3"
+alias pyman="python3 manage.py"
+alias pyrun="python3 manage.py runserver"
+alias pytest="python3 manage.py test --keepdb --exclude-tag=external"
+
+export HUSKY_SKIP_INSTALL=true
